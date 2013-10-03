08648778cc802113c54a0b81e46b5afc813b317d
Quinn Slack <qslack@qslack.com>
2013-10-03 03:51:00 -0700

    Fallback.WaitNWrites

--- fallback.go
+++ fallback.go
@@ -2,11 +2,12 @@
 // short-circuits gets and writes/deletes to all underlying caches.
 package multicache
 
-// Fallback is a cache that wraps a list of caches. Sets write to all caches.
-// Gets read from the caches in sequence until a cache entry is found. Deletes
-// delete from all caches.
+// Fallback is a cache that wraps a list of caches. Sets write to the first
+// WaitNWrites caches. Gets read from the caches in sequence until a cache entry
+// is found. Deletes delete from all caches.
 type Fallback struct {
-	caches []Underlying
+	caches      []Underlying
+	WaitNWrites int
 }
 
 func (f *Fallback) Get(key string) (resp []byte, ok bool) {
@@ -20,8 +21,12 @@ func (f *Fallback) Get(key string) (resp []byte, ok bool) {
 }
 
 func (f *Fallback) Set(key string, resp []byte) {
-	for _, c := range f.caches {
-		c.Set(key, resp)
+	for i, c := range f.caches {
+		if i < f.WaitNWrites {
+			c.Set(key, resp)
+		} else {
+			go c.Set(key, resp)
+		}
 	}
 }
 
@@ -31,7 +36,7 @@ func (f *Fallback) Delete(key string) {
 	}
 }
 
-// NewFallback returns a new Fallback cache.
+// NewFallback returns a new Fallback cache with WaitNWrites == len(caches).
 func NewFallback(caches ...Underlying) *Fallback {
-	return &Fallback{caches: caches}
+	return &Fallback{caches: caches, WaitNWrites: len(caches)}
 }
